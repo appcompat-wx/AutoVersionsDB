93b1fd2b8a88d4c8cdbc3eaab45d7ba4add7c9c0
appcompat-wx <appcompatwx@microsoft.com>
2026-04-22 10:01:46 +0800

    upload code from lab

---
+++ src/AutoVersionsDB/AutoVersionsDB.Core/AutoVersionsDbAPI.cs
@@ -0,0 +1,229 @@
+﻿using AutoVersionsDB.Core.ConfigProjects;
+using AutoVersionsDB.Core.DBVersions;
+using AutoVersionsDB.Core.DBVersions.Processes;
+using AutoVersionsDB.DB.Contract;
+using AutoVersionsDB.NotificationableEngine;
+using Ninject;
+using System;
+using System.Collections.Generic;
+
+namespace AutoVersionsDB.Core
+{
+    public static class AutoVersionsDBAPI
+    {
+
+        private static readonly object _processSyncLock = new object();
+
+
+        private static readonly ProjectConfigsAPI _projectConfigsAPI = DIConfig.Kernel.Get<ProjectConfigsAPI>();
+        private static readonly DBVersionsAPI _dbVersionsAPI = DIConfig.Kernel.Get<DBVersionsAPI>();
+
+
+
+
+
+        #region Config
+
+        public static IList<DBType> DBTypes => _projectConfigsAPI.DBTypes;
+
+
+        public static List<ProjectConfigItem> GetProjectsList()
+        {
+            lock (_processSyncLock)
+            {
+                return _projectConfigsAPI.GetProjectsList();
+            }
+        }
+
+        public static ProjectConfigItem GetProjectConfigById(string id)
+        {
+            lock (_processSyncLock)
+            {
+                return _projectConfigsAPI.GetProjectConfigById(id);
+            }
+        }
+
+
+
+        public static ProcessResults SaveNewProjectConfig(ProjectConfigItem projectConfig, Action<ProcessTrace, StepNotificationState> onNotificationStateChanged)
+        {
+            lock (_processSyncLock)
+            {
+                return _projectConfigsAPI.SaveNewProjectConfig(projectConfig, onNotificationStateChanged);
+            }
+        }
+
+        public static ProcessResults UpdateProjectConfig(ProjectConfigItem projectConfig, Action<ProcessTrace, StepNotificationState> onNotificationStateChanged)
+        {
+            lock (_processSyncLock)
+            {
+                return _projectConfigsAPI.UpdateProjectConfig(projectConfig, onNotificationStateChanged);
+            }
+        }
+
+        public static ProcessResults ChangeProjectId(string prevId, string newId, Action<ProcessTrace, StepNotificationState> onNotificationStateChanged)
+        {
+            lock (_processSyncLock)
+            {
+                return _projectConfigsAPI.ChangeProjectId(prevId, newId, onNotificationStateChanged);
+            }
+        }
+
+        public static ProcessResults RemoveProjectConfig(string id, Action<ProcessTrace, StepNotificationState> onNotificationStateChanged)
+        {
+            lock (_processSyncLock)
+            {
+                return _projectConfigsAPI.RemoveProjectConfig(id, onNotificationStateChanged);
+            }
+        }
+
+
+
+
+
+
+        #endregion
+
+
+        #region Validation
+
+        public static ProcessResults ValidateProjectConfig(ProjectConfigItem projectConfig, Action<ProcessTrace, StepNotificationState> onNotificationStateChanged)
+        {
+            lock (_processSyncLock)
+            {
+                return _projectConfigsAPI.ValidateProjectConfig(projectConfig, onNotificationStateChanged);
+            }
+        }
+
+        public static ProcessResults ValidateDBVersions(string id, Action<ProcessTrace, StepNotificationState> onNotificationStateChanged)
+        {
+            lock (_processSyncLock)
+            {
+                return _dbVersionsAPI.ValidateDBVersions(id, onNotificationStateChanged);
+            }
+        }
+
+        public static ProcessResults ValdiateTargetStateAlreadyExecuted(string id, string targetStateScriptFilename, Action<ProcessTrace, StepNotificationState> onNotificationStateChanged)
+        {
+            lock (_processSyncLock)
+            {
+                return _dbVersionsAPI.ValdiateTargetStateAlreadyExecuted(id, targetStateScriptFilename, onNotificationStateChanged);
+            }
+        }
+
+        #endregion
+
+
+        #region Run Change DB State
+
+        public static ProcessResults SyncDB(string id, Action<ProcessTrace, StepNotificationState> onNotificationStateChanged)
+        {
+            lock (_processSyncLock)
+            {
+                return _dbVersionsAPI.SyncDB(id, onNotificationStateChanged);
+            }
+        }
+
+        public static ProcessResults RecreateDBFromScratch(string id, TargetScripts targetScripts, Action<ProcessTrace, StepNotificationState> onNotificationStateChanged)
+        {
+            lock (_processSyncLock)
+            {
+                return _dbVersionsAPI.RecreateDBFromScratch(id, targetScripts, onNotificationStateChanged);
+            }
+        }
+
+
+        public static ProcessResults SetDBToSpecificState(string id, TargetScripts targetScripts, bool isIgnoreHistoryWarning, Action<ProcessTrace, StepNotificationState> onNotificationStateChanged)
+        {
+            lock (_processSyncLock)
+            {
+                return _dbVersionsAPI.SetDBToSpecificState(id, targetScripts, isIgnoreHistoryWarning, onNotificationStateChanged);
+            }
+        }
+
+
+        public static ProcessResults SetDBStateByVirtualExecution(string id, TargetScripts targetScripts, Action<ProcessTrace, StepNotificationState> onNotificationStateChanged)
+        {
+            lock (_processSyncLock)
+            {
+                return _dbVersionsAPI.SetDBStateByVirtualExecution(id, targetScripts, onNotificationStateChanged);
+            }
+        }
+
+        public static ProcessResults VirtualDDD(string id, Action<ProcessTrace, StepNotificationState> onNotificationStateChanged)
+        {
+            lock (_processSyncLock)
+            {
+                return _dbVersionsAPI.VirtualDDD(id, onNotificationStateChanged);
+            }
+        }
+
+        public static ProcessResults InitDB(string id, Action<ProcessTrace, StepNotificationState> onNotificationStateChanged)
+        {
+            lock (_processSyncLock)
+            {
+                return _dbVersionsAPI.InitDB(id, onNotificationStateChanged);
+            }
+        }
+
+
+        #endregion
+
+
+        #region Deploy
+
+        public static ProcessResults Deploy(string id, Action<ProcessTrace, StepNotificationState> onNotificationStateChanged)
+        {
+            lock (_processSyncLock)
+            {
+                return _dbVersionsAPI.Deploy(id, onNotificationStateChanged);
+            }
+        }
+
+        #endregion
+
+
+
+        #region Scripts
+
+        public static ProcessResults GetScriptFilesState(string id, Action<ProcessTrace, StepNotificationState> onNotificationStateChanged)
+        {
+            lock (_processSyncLock)
+            {
+                return _dbVersionsAPI.GetScriptFilesState(id, onNotificationStateChanged);
+            }
+        }
+
+        public static ProcessResults CreateNewIncrementalScriptFile(string id, string scriptName, Action<ProcessTrace, StepNotificationState> onNotificationStateChanged)
+        {
+            lock (_processSyncLock)
+            {
+                return _dbVersionsAPI.CreateNewIncrementalScriptFile(id, scriptName, onNotificationStateChanged);
+            }
+        }
+
+        public static ProcessResults CreateNewRepeatableScriptFile(string id, string scriptName, Action<ProcessTrace, StepNotificationState> onNotificationStateChanged)
+        {
+            lock (_processSyncLock)
+            {
+                return _dbVersionsAPI.CreateNewRepeatableScriptFile(id, scriptName, onNotificationStateChanged);
+            }
+        }
+
+        public static ProcessResults CreateNewDevDummyDataScriptFile(string id, string scriptName, Action<ProcessTrace, StepNotificationState> onNotificationStateChanged)
+        {
+            lock (_processSyncLock)
+            {
+                return _dbVersionsAPI.CreateNewDevDummyDataScriptFile(id, scriptName, onNotificationStateChanged);
+            }
+        }
+
+
+        #endregion
+
+
+
+
+
+    }
+}
---
+++ src/AutoVersionsDB/AutoVersionsDB.WinApp/ProjectItemControl.cs
@@ -0,0 +1,124 @@
+﻿using AutoVersionsDB.Core.ConfigProjects;
+using AutoVersionsDB.UI.ChooseProject;
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace AutoVersionsDB.WinApp
+{
+    public partial class ProjectItemControl : UserControl
+    {
+        private readonly ChooseProjectViewModel _viewModel;
+
+
+        public ProjectConfigItem ProjectConfig { get; private set; }
+
+        //public ProjectItemControl(ProjectConfigItem projectConfigItem)
+        public ProjectItemControl(ChooseProjectViewModel viewModel, ProjectConfigItem projectConfig)
+        {
+            InitializeComponent();
+
+            _viewModel = viewModel;
+            ProjectConfig = projectConfig;
+
+            //if (LicenseManager.UsageMode != LicenseUsageMode.Designtime)
+            //{
+            //    _viewModel.PropertyChanged += ViewModel_PropertyChanged;
+            //    SetDataBindings();
+            //}
+
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            if (!this.DesignMode)
+            {
+                _viewModel.PropertyChanged += ViewModel_PropertyChanged;
+                SetDataBindings();
+            }
+        }
+
+
+
+
+        private void ViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                default:
+                    break;
+            }
+        }
+
+
+        private void SetDataBindings()
+        {
+            lblId.Text = ProjectConfig.Id;
+            lblProjectDesc.Text = ProjectConfig.Description;
+        }
+
+
+
+        private void LblProjectName_Click(object sender, EventArgs e)
+        {
+            _viewModel.NavToDBVersionsCommand.Execute(ProjectConfig.Id);
+        }
+
+        private void LblProjectIcon_Click(object sender, EventArgs e)
+        {
+            _viewModel.NavToDBVersionsCommand.Execute(ProjectConfig.Id);
+        }
+
+        private void LblProcessLink_Click(object sender, EventArgs e)
+        {
+            _viewModel.NavToDBVersionsCommand.Execute(ProjectConfig.Id);
+        }
+
+        private void LblEditProject_Click(object sender, EventArgs e)
+        {
+            _viewModel.NavToEditProjectConfigCommand.Execute(ProjectConfig.Id);
+        }
+
+        private void LblDeleteProject_Click(object sender, EventArgs e)
+        {
+            _viewModel.DeleteProjectCommand.Execute(ProjectConfig.Id);
+        }
+
+
+
+
+        #region Dispose
+
+        // To detect redundant calls
+        private bool _disposed;
+
+        // Protected implementation of Dispose pattern.
+        protected override void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                // Dispose managed state (managed objects).
+
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+            }
+
+            _disposed = true;
+            // Call base class implementation.
+            base.Dispose(disposing);
+        }
+
+        #endregion
+
+
+    }
+}
